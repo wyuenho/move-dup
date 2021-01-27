bf2e578b89d7e7bf0b5500d9afcf49ac6ec2dcd1
Jimmy Yuen Ho Wong <wyuenho@gmail.com>
2021-01-27 19:38:34 +0000

    lint

--- move-dup.el
+++ move-dup.el
@@ -1,6 +1,6 @@
-;;; move-dup.el --- Eclipse-like moving and duplicating lines or rectangles.
+;;; move-dup.el --- Eclipse-like moving and duplicating lines or rectangles
 
-;; Copyright (C) 2014-2020 Jimmy Yuen Ho Wong
+;; Copyright (C) 2014-2021 Jimmy Yuen Ho Wong
 
 ;; This program is free software: you can redistribute it and/or modify it under
 ;; the terms of the GNU General Public License as published by the Free Software
@@ -16,10 +16,10 @@
 ;; this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;; Author: Jimmy Yuen Ho Wong <wyuenho@gmail.com>
-;; Version: 1.1.2
+;; Version: 2.0.0
 ;; Created: 11 June 2014
 ;; Url: https://github.com/wyuenho/move-dup
-;; Compatibility: GNU Emacs 25.1
+;; Package-Requires: ((emacs "25.1"))
 ;; Keywords: convenience text edit
 
 ;; This file is not part of GNU Emacs.
@@ -39,10 +39,10 @@
 ;; If you don't want to toggle the minor mode, you can bind these functions like
 ;; so.  All of these functions work on a single line or a rectangle.
 
-;; (global-set-key (kbd "M-<up>") 'md-move-lines-up)
-;; (global-set-key (kbd "M-<down>") 'md-move-lines-down)
-;; (global-set-key (kbd "C-M-<up>") 'md-duplicate-up)
-;; (global-set-key (kbd "C-M-<down>") 'md-duplicate-down)
+;; (global-set-key (kbd "M-<up>") 'move-dup-move-lines-up)
+;; (global-set-key (kbd "M-<down>") 'move-dup-move-lines-down)
+;; (global-set-key (kbd "C-M-<up>") 'move-dup-duplicate-up)
+;; (global-set-key (kbd "C-M-<down>") 'move-dup-duplicate-down)
 
 ;; If you used `package.el' to install `move-dup.el', this is equivalent to all
 ;; of the above.
@@ -63,11 +63,12 @@
   :type 'boolean
   :group 'move-dup)
 
-(defun md-merge-undo-boundaries-maybe ()
+(defun move-dup-merge-undo-boundaries-maybe ()
   (when move-dup-undo-auto-amalgamate
     (undo-auto-amalgamate)))
+(make-obsolete 'md-merge-undo-boundaries-maybe 'move-dup-merge-undo-boundaries-maybe "2.0.0")
 
-(defun md-ensure-rectangle ()
+(defun move-dup-ensure-rectangle ()
   "Normalizes the selection by making sure it's always a rectangle.
 
 After normalization, the point always comes after mark.  The
@@ -86,18 +87,19 @@ beginning of the next line of the end of the region."
     (exchange-point-and-mark)
     (beginning-of-line)
     (exchange-point-and-mark)))
+(make-obsolete 'md-ensure-rectangle 'move-dup-ensure-rectangle "2.0.0")
 
 ;;;###autoload
-(defun md-move-region (&optional n)
+(defun move-dup-move-region (&optional n)
   "Interactive function to move the current selection N lines.
 
 If the selection is not a rectangle, this function will expand
 the selection to a rectangle via the function
-`md-ensure-rectangle' and move it accordingly.  If the prefix N
+`move-dup-ensure-rectangle' and move it accordingly.  If the prefix N
 is positive, this function moves the rectangle forward N lines;
 otherwise backward."
   (interactive "*p")
-  (md-ensure-rectangle)
+  (move-dup-ensure-rectangle)
   (let* ((start (region-beginning))
          (end (region-end)))
     (if (< n 0)
@@ -114,9 +116,10 @@ otherwise backward."
       (push-mark)
       (backward-char (- end start))
       (exchange-point-and-mark))))
+(make-obsolete 'md-move-region 'move-dup-move-region "2.0.0")
 
 ;;;###autoload
-(defun md-move-line (&optional n)
+(defun move-dup-move-line (&optional n)
   "Interactive function to move the current line by N lines.
 
 If the prefix N is positive, this function moves the current line
@@ -126,65 +129,72 @@ forward N lines; otherwise backward."
     (goto-char (save-mark-and-excursion
                  (push-mark)
                  (forward-line)
-                 (md-move-region n)
+                 (move-dup-move-region n)
                  (region-beginning)))
     (move-to-column col)))
+(make-obsolete 'md-move-line 'move-dup-move-line "2.0.0")
 
-(defun md-move-line-or-region (n)
+(defun move-dup-move-line-or-region (n)
   "Decides whether a line or selection should be moved N lines."
-  (md-merge-undo-boundaries-maybe)
+  (move-dup-merge-undo-boundaries-maybe)
   (if (use-region-p)
-      (md-move-region n)
-    (md-move-line n)))
+      (move-dup-move-region n)
+    (move-dup-move-line n)))
+(make-obsolete 'md-move-line-or-region 'move-dup-move-line-or-region "2.0.0")
 
 ;;;###autoload
-(defun md-move-lines-up (&optional n)
+(defun move-dup-move-lines-up (&optional n)
   "Interactive function to move the current line or selection up.
 
 If the prefix N is positive, this function moves the current line
 or selection up N lines; otherwise down."
   (interactive "*p")
-  (md-move-line-or-region (if (or (null n) (= n 0)) -1 (- n))))
+  (move-dup-move-line-or-region (if (or (null n) (= n 0)) -1 (- n))))
+(make-obsolete 'md-move-lines-up 'move-dup-move-lines-up "2.0.0")
 
 ;;;###autoload
-(defun md-move-lines-down (&optional n)
+(defun move-dup-move-lines-down (&optional n)
   "Interactive function to move the current line or selection down.
 
 If the prefix N is positive, this function moves the current line
 or selection down N lines; otherwise up."
   (interactive "*p")
-  (md-move-line-or-region (if (or (null n) (= n 0)) 1 n)))
+  (move-dup-move-line-or-region (if (or (null n) (= n 0)) 1 n)))
+(make-obsolete 'md-move-lines-down 'move-dup-move-lines-down "2.0.0")
 
 ;;;###autoload
-(defun md-duplicate-up (&optional n)
+(defun move-dup-duplicate-up (&optional n)
   "Interactive function to duplicate the current line or selection upward.
 
 If the prefix N is positive, this function makes N duplicates of
 the current line or selection and place them above the current
 line or selection."
   (interactive "*p")
-  (dotimes (i n) (md-duplicate-line-or-region "up")))
+  (dotimes (i n) (move-dup-duplicate-line-or-region "up")))
+(make-obsolete 'md-duplicate-up 'move-dup-duplicate-up "2.0.0")
 
 ;;;###autoload
-(defun md-duplicate-down (&optional n)
+(defun move-dup-duplicate-down (&optional n)
   "Interactive function to duplicate the current line or selection downward.
 
 If the prefix N is positive, this function makes N duplicates of
 the current line or selection and place them below the current
 line or selection."
   (interactive "*p")
-  (dotimes (i n) (md-duplicate-line-or-region "down")))
+  (dotimes (i n) (move-dup-duplicate-line-or-region "down")))
+(make-obsolete 'md-duplicate-down 'move-dup-duplicate-down "2.0.0")
 
-(defun md-duplicate-line-or-region (direction)
+(defun move-dup-duplicate-line-or-region (direction)
   "Decides whether a line or selection should be duplicated.
 
 DIRECTION must be one of \"up\" or \"down\"."
-  (md-merge-undo-boundaries-maybe)
+  (move-dup-merge-undo-boundaries-maybe)
   (if (use-region-p)
-      (md-duplicate-region direction)
-    (md-duplicate-line direction)))
+      (move-dup-duplicate-region direction)
+    (move-dup-duplicate-line direction)))
+(make-obsolete 'md-duplicate-line-or-region 'move-dup-duplicate-line-or-region "2.0.0")
 
-(defun md-duplicate-line (direction)
+(defun move-dup-duplicate-line (direction)
   "Function to duplicate the current line.
 
 DIRECTION must be one of \"up\" or \"down\"."
@@ -198,19 +208,20 @@ DIRECTION must be one of \"up\" or \"down\"."
     (if (string= direction "up")
         (forward-line -1))
     (move-to-column col)))
+(make-obsolete 'md-duplicate-line 'move-dup-duplicate-line "2.0.0")
 
-(defun md-duplicate-region (direction)
+(defun move-dup-duplicate-region (direction)
   "Function to duplicate the current selection.
 
 DIRECTION must be one of \"up\" or \"down\".
 
 If the selection is not a rectangle, this function will expand
 the selection to a rectangle via the function
-`md-ensure-rectangle' and duplicate it accordingly.  If the
+`move-dup-ensure-rectangle' and duplicate it accordingly.  If the
 DIRECTION is \"up\", this function duplicates the selected
 rectangle and places it __below__ the selection; __above__ if
 DIRECTION is \"down\"."
-  (md-ensure-rectangle)
+  (move-dup-ensure-rectangle)
   (let* ((start (region-beginning))
          (end (region-end))
          (text (buffer-substring start end))
@@ -224,6 +235,7 @@ DIRECTION is \"down\"."
            (exchange-point-and-mark))
           ((string= direction "up")
            (backward-char text-length)))))
+(make-obsolete 'md-duplicate-region 'move-dup-duplicate-region "2.0.0")
 
 ;;;###autoload
 (define-minor-mode move-dup-mode
@@ -232,16 +244,16 @@ rectangles with default key bindings.
 
 The default key bindings are:
 
-\([M-up] . md-move-lines-up)
-\([M-down] . md-move-lines-down)
-\([C-M-up] . md-duplicate-up)
-\([C-M-down] . md-duplicate-down)"
+\([M-up]     . move-dup-move-lines-up)
+\([M-down]   . move-dup-move-lines-down)
+\([C-M-up]   . move-dup-duplicate-up)
+\([C-M-down] . move-dup-duplicate-down)"
   :lighter " md"
   :group 'move-dup
-  :keymap '(([M-up] . md-move-lines-up)
-            ([M-down] . md-move-lines-down)
-            ([C-M-up] . md-duplicate-up)
-            ([C-M-down] . md-duplicate-down)))
+  :keymap '(([M-up]     . move-dup-move-lines-up)
+            ([M-down]   . move-dup-move-lines-down)
+            ([C-M-up]   . move-dup-duplicate-up)
+            ([C-M-down] . move-dup-duplicate-down)))
 
 (defun move-dup-on ()
   "Decides whether the function `move-dup-mode' should be called with t."
